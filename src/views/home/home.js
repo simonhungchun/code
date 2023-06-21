a30029fa2aa22e4655e1e497078406fe3feb8368
Simon Hungchun <cc_0820@163.com>
2023-06-21 14:59:23 +0800

    chore: 配置webpack多页应用打包
    webpack.config.js

---
+++ src/views/home/home.js
@@ -0,0 +1,10 @@
+import a from './a';
+import './home.less';
+// import axios from 'axios';
+// axios
+//   .get('/bff/app-provision-api/default/banner', {
+//     params: {
+//       bannerType: 'tmall-banner',
+//     },
+//   })
+//   .then((res) => console.log(res));
